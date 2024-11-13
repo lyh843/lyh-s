b619d937728cc6779066bb40d4f3e4e663697c02
lyh <2641805259@qq.com>
2024-11-13 23:55:37 +0800

    2024.11.13_2

--- hw_24_6/b.c
+++ hw_24_6/b.c
@@ -2,9 +2,53 @@
 #include<string.h>
 #include<stdbool.h>
 #define N 100001
+void f_push(char , bool *, char *, int *);
+void f_judge(char *, bool *);
 void f_judge(char *pt_string, bool *pt_judge)
 {
-
+    int len = strlen(pt_string), len_stack = 0;
+    char stack[N];
+    memset(stack, 0, sizeof(stack));
+    for(int i = 0; i < len; i++)
+    {
+        f_push(*(pt_string + i), pt_judge, stack, &len_stack);
+    }
+    if(len_stack == 0)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+}
+void f_push(char pt_push, bool *pt_judge, char *pt_stack, int *pt_len_stack)
+{
+    if(pt_push == '(' || pt_push == '{' || pt_push == '[')
+    {
+        (*pt_len_stack)++;
+        *(pt_stack + *pt_len_stack - 1) = pt_push;
+    }
+    else
+    {
+        if(pt_push == ')' && *(pt_stack + *pt_len_stack - 1) == '(')
+        {
+            (*pt_len_stack)--;
+        }
+        else if(pt_push == '}' && *(pt_stack + *pt_len_stack - 1) == '{')
+        {
+            (*pt_len_stack)--;
+        }
+        else if(pt_push == ']' && *(pt_stack + *pt_len_stack - 1) == '[')
+        {
+            (*pt_len_stack)--;
+        }
+        else
+        {
+            (*pt_len_stack)++;
+            *(pt_stack + *pt_len_stack - 1) = pt_push;
+        }
+    }
 }
 int main(void)
 {
